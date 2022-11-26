f44041d3afd8a3942099c0017243ec7829a8eb71
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-11-26 11:25:56 +0700

    feat: add conditional dialog popup

--- app/src/main/java/com/kelompoktiga/kotdota/activity/SearchActivity.kt
+++ app/src/main/java/com/kelompoktiga/kotdota/activity/SearchActivity.kt
@@ -3,8 +3,12 @@ package com.kelompoktiga.kotdota.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,27 +32,35 @@ class SearchActivity : AppCompatActivity() {
                 it.localizedName!!.lowercase() == searchName.lowercase()
             }
 
+            if (searchedHeroes.isEmpty()) {
+                findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+                findViewById<LinearLayout>(R.id.empty_search_dialog).visibility = View.VISIBLE
+            } else {
+
+
+                val rvHeroes = findViewById<RecyclerView>(R.id.rv_heroes)
+                rvHeroes.setHasFixedSize(true)
+
+                rvHeroes.layoutManager = GridLayoutManager(this, 3)
+                val heroGridAdapter = HeroGridAdapter(searchedHeroes.toMutableList())
+                rvHeroes.adapter = heroGridAdapter
+
+                heroGridAdapter.setOnItemClickCallback(object :
+                    HeroGridAdapter.OnItemClickCallback {
+                    override fun onItemClicked(pos: Int) {
+                        val heroDetailsIntent =
+                            Intent(this@SearchActivity, HeroDetailsActivity::class.java)
+                        heroDetailsIntent.putExtra("id", pos.toString())
+                        startActivity(heroDetailsIntent)
+                    }
+                })
+                findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+            }
             findViewById<TextView>(R.id.search_name).text = searchName
             findViewById<ImageButton>(R.id.btn_back).setOnClickListener {
-                val homeIntent = Intent(this, HomeActivity::class.java)
-                startActivity(homeIntent)
+                finish()
+                Log.d("chch","ye")
             }
-
-            val rvHeroes = findViewById<RecyclerView>(R.id.rv_heroes)
-            rvHeroes.setHasFixedSize(true)
-
-            rvHeroes.layoutManager = GridLayoutManager(this, 3)
-            val heroGridAdapter = HeroGridAdapter(searchedHeroes.toMutableList())
-            rvHeroes.adapter = heroGridAdapter
-
-            heroGridAdapter.setOnItemClickCallback(object : HeroGridAdapter.OnItemClickCallback {
-                override fun onItemClicked(pos: Int) {
-                    val heroDetailsIntent =
-                        Intent(this@SearchActivity, HeroDetailsActivity::class.java)
-                    heroDetailsIntent.putExtra("id", pos.toString())
-                    startActivity(heroDetailsIntent)
-                }
-            })
         }
     }
 }
